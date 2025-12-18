1a1c7b956c93e3aeef62776f2c2b1cb1113c77c8
Alex Page <alex@anpage.me>
2025-12-17 20:29:03 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+#![no_main]
+#![no_std]
+
+#[macro_use]
+mod macros;
+mod keymap;
+mod vial;
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_stm32::flash::Flash;
+use embassy_stm32::gpio::{Input, Output};
+use embassy_stm32::peripherals::USB_OTG_FS;
+use embassy_stm32::usb::{Driver, InterruptHandler};
+use embassy_stm32::{Config, bind_interrupts};
+use keymap::{COL, ROW};
+use rmk::channel::EVENT_CHANNEL;
+use rmk::config::{BehaviorConfig, PositionalConfig, RmkConfig, StorageConfig, VialConfig};
+use rmk::debounce::default_debouncer::DefaultDebouncer;
+use rmk::futures::future::join3;
+use rmk::input_device::Runnable;
+use rmk::keyboard::Keyboard;
+use rmk::matrix::Matrix;
+use rmk::storage::async_flash_wrapper;
+use rmk::{initialize_keymap_and_storage, run_devices, run_rmk};
+use static_cell::StaticCell;
+use vial::{VIAL_KEYBOARD_DEF, VIAL_KEYBOARD_ID};
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    OTG_FS => InterruptHandler<USB_OTG_FS>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("RMK start!");
+    // RCC config
+    let config = Config::default();
+
+    // Initialize peripherals
+    let p = embassy_stm32::init(config);
+
+    // Usb config
+    static EP_OUT_BUFFER: StaticCell<[u8; 1024]> = StaticCell::new();
+    let mut usb_config = embassy_stm32::usb::Config::default();
+    usb_config.vbus_detection = false;
+    let driver = Driver::new_fs(
+        p.USB_OTG_FS,
+        Irqs,
+        p.PA12,
+        p.PA11,
+        &mut EP_OUT_BUFFER.init([0; 1024])[..],
+        usb_config,
+    );
+
+    // Pin config
+    let (row_pins, col_pins) = config_matrix_pins_stm32!(peripherals: p, input: [PA9, PB8, PB13, PB12], output: [PA13, PA14, PA15]);
+
+    // Use internal flash to emulate eeprom
+    let flash = async_flash_wrapper(Flash::new_blocking(p.FLASH));
+
+    // Keyboard config
+    let rmk_config = RmkConfig {
+        vial_config: VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF, &[(0, 0), (1, 1)]),
+        ..Default::default()
+    };
+
+    // Initialize the storage and keymap
+    let mut default_keymap = keymap::get_default_keymap();
+    let mut behavior_config = BehaviorConfig::default();
+    let storage_config = StorageConfig::default();
+    let mut per_key_config = PositionalConfig::default();
+    let (keymap, mut storage) = initialize_keymap_and_storage(
+        &mut default_keymap,
+        flash,
+        &storage_config,
+        &mut behavior_config,
+        &mut per_key_config,
+    )
+    .await;
+
+    // Initialize the matrix + keyboard
+    let debouncer = DefaultDebouncer::new();
+    let mut matrix = Matrix::<_, _, _, ROW, COL, true>::new(row_pins, col_pins, debouncer);
+    let mut keyboard = Keyboard::new(&keymap);
+
+    // Start
+    join3(
+        run_devices! (
+            (matrix) => EVENT_CHANNEL,
+        ),
+        keyboard.run(),
+        run_rmk(&keymap, driver, &mut storage, rmk_config),
+    )
+    .await;
+}
