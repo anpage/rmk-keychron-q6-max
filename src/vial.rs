59913736acb81303378ce951855e4ee01a1e3c9d
Alex Page <alex@anpage.me>
2025-12-18 21:18:39 -0500

    Add copyright notices to source files

--- src/vial.rs
+++ src/vial.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2025 Alex Page
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 pub const VIAL_KEYBOARD_ID: &'static [u8] = &[54u8, 55u8, 38u8, 119u8, 170u8, 69u8, 203u8, 89u8];
 pub const VIAL_KEYBOARD_DEF: &'static [u8] = &[
     253u8, 55u8, 122u8, 88u8, 90u8, 0u8, 0u8, 4u8, 230u8, 214u8, 180u8, 70u8, 2u8, 0u8, 33u8, 1u8,
